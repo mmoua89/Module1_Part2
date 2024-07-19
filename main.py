fd9ba73909145c096a52ab0dec61c3b968f339df
mmoua89 <mmoua.89@outlook.com>
2024-07-19 09:13:02 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Author: Meng Moua
+Course: CSC500-1
+Assignment: Part 2
+    - Write a Python program to find the multiplication and division of two numbers.
+    - Ask the user to input two numbers (num1 and num2). Given those two numbers,
+      multiply them together to find the output. Also, divide num1/num2 to find the output.
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
